7734662e9d7e432ba86f10d61fd13987c4564777
m-gouda <goda61@gmail.com>
2023-09-21 13:57:59 +0200

    Add examples

---
+++ Examples files/11-Python Intermediate P1.py
@@ -0,0 +1,6 @@
+names = ['ahmed','ali','mahmoud']
+new_names = []
+for x in names :
+    new_names.append(x.upper())
+
+print(new_names)
--- Examples files/test.py
+++ Examples files/test.py
@@ -1,22 +1,31 @@
-class Students :
-    def __init__(self,name):
-        print(f'Welcome {name}')
-        self.marks = []
-    def add_mark (self,mark):
-        self.marks.append(mark)
-        print (self.marks)
-    def get_avg (self):
-        print(sum(self.marks)/len(self.marks))
+'''
+odd_numbers =[]
+even_numbers = []
 
-    
+def odd_even (x,y) :
+    for n in range(x,y) :
+        if n%2 == 0 :
+            odd_numbers.append(n)
+        elif n%2 != 0 :
+            even_numbers.append(n)
+
+    print(f'Odd Number = {odd_numbers}')
+    print(f'Even Number = {even_numbers}')
+
+odd_even (100,200)
+'''
+
+'''
+def divided_numbers (x,y) :
+    for n in range (1,101) :
+        if n%x ==0 and n%y ==0 :
+            print (n)
         
-    
-S1 = Students('Ahmed')
-S1.add_mark(100)
-S1.add_mark(120)
-S1.add_mark(110)
-S1.add_mark(130)
-S1.get_avg()
+divided_numbers(5,6)
+'''
 
+def muli_table (x,y):
+    for n in range(x,y) :
+        print (x)
 
-
+muli_table(1,10)
