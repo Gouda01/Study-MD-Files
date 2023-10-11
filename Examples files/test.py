cb1d7bee3463af47f2abda16b180522a6d73a065
Gouda01 <goda61@gmail.com>
2023-10-11 19:20:16 +0300

    Add new examples

--- Examples files/test.py
+++ Examples files/test.py
@@ -32,6 +32,8 @@ def muli_table (x,y):
 muli_table(1,10)
 '''
 
+'''
+
 names = ['ahmed','ali','mahmoud','aid','hassan']
 
 # Normal List :
@@ -51,3 +53,105 @@ print(new_names)
 print(new_names1)
 print(new_names2)
 print(new_names3)
+
+'''
+
+'''
+
+class StudentDetails :
+    def add_student (self,name):
+        self.mark = []
+        print(f'Welcome {name}')
+
+    def add_mark(self,mark) :
+        self.mark.append(mark)
+        print(self.mark)
+
+    def mark_avg (self):
+        mark_avg = sum(self.mark) / len(self.mark)
+        print(mark_avg)
+
+s1 = StudentDetails()
+s1.add_student('Mohamed')
+s1.add_mark (20)
+s1.add_mark (30)
+s1.add_mark (50)
+s1.mark_avg()
+'''
+
+'''
+class Bank :
+    def __init__(self,name,age) :
+        print(f'Welcome {name}')
+        self.name = name
+        self.age = age
+        self.balance = 0
+    
+    def deposite (self,amount) :
+        self.balance += amount
+        print(f'The deposite done successful and your balance is : {self.balance}')
+    
+    def withdraw (self,amount) :
+        if self.balance >= amount :
+            self.balance -= amount
+            print(f'The withdraw done successful and your balance is : {self.balance}')
+        else :
+            print('The withdraw not done because your balance not allow')
+    
+    def show_details (self):
+        print(f
+    Customer Name       : {self.name}
+    Customer age        : {self.age}
+    Customer balance    : {self.balance}
+
+#)
+
+
+c1 = Bank('Mohamed',21)
+c1.deposite(5000)
+c1.deposite(10000)
+c1.withdraw(7000)
+c1.show_details()
+
+'''
+class Customers :
+    def __init__(self,name,age) :
+        print(f'Welcome {name}')
+        self.name = name
+        self.age = age
+        
+
+    def show_details (self):
+        print(f'''
+    Customer Name       : {self.name}
+    Customer age        : {self.age}
+    Customer balance    : {self.balance}
+
+''')
+
+
+
+class Bank(Customers) :
+
+    def __init__(self,name,age) :
+        super().__init__(name,age)
+        self.balance = 0
+
+    def deposite (self,amount) :
+        self.balance += amount
+        print(f'The deposite done successful and your balance is : {self.balance}')
+    
+    def withdraw (self,amount) :
+        if self.balance >= amount :
+            self.balance -= amount
+            print(f'The withdraw done successful and your balance is : {self.balance}')
+        else :
+            print('The withdraw not done because your balance not allow')
+
+
+
+c1 = Bank('Mohamed',21)
+c1.deposite(5000)
+c1.deposite(10000)
+c1.withdraw(7000)
+c1.show_details()
